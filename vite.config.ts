e9657c414164d25f813d435217d2927b83611c18
Anderson_PC35 <anderson.loiro222@gmail.com>
2025-07-11 15:08:56 -0300

    Fix: Ajusta url base para o nome do repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/dist/',
+  base: "/teste-lovable/",
   server: {
     host: "::",
     port: 8080,
